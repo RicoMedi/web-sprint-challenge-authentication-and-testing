b0f31b85e110e0b14ac262d04c86d5e70e3afb67
Madcodes831 <r.medina2119@gmail.com>
2024-01-17 17:36:28 -0800

    trying to figure out err message

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -26,12 +26,6 @@ router.post(
   async (req, res, next) => {
     try {
       const { username, password } = req.body;
-
-      // Check if both username and password are provided
-      if (!username || !password) {
-        return res.status(400).json({ message: "username and password required" });
-      }
-
       const newUser = {
         username: username,
         password: await bcrypt.hash(password, BC_ROUNDS),
@@ -43,9 +37,34 @@ router.post(
     } catch (err) {
       next(err);
     }
+
+    /*
+    IMPLEMENT
+    You are welcome to build additional middlewares to help with the endpoint's functionality.
+    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+
+    1- In order to register a new account the client must provide `username` and `password`:
+      {
+        "username": "Captain Marvel", // must not exist already in the `users` table
+        "password": "foobar"          // needs to be hashed before it's saved
+      }
+
+    2- On SUCCESSFUL registration,
+      the response body should have `id`, `username` and `password`:
+      {
+        "id": 1,
+        "username": "Captain Marvel",
+        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
+      }
+
+    3- On FAILED registration due to `username` or `password` missing from the request body,
+      the response body should include a string exactly as follows: "username and password required".
+
+    4- On FAILED registration due to the `username` being taken,
+      the response body should include a string exactly as follows: "username taken".
+  */
   }
 );
-
 router.post("/login", checkSubmission, async (req, res, next) => {
   try {
     let { username, password } = req.body;
@@ -62,7 +81,7 @@ console.log(token)
         
       });
     } else {
-      res.status(401).json({ message: 'username and password required' });
+      res.status(401).json({ message: 'invalid credentials' });
     }
   } catch (err) {
     next(err);
