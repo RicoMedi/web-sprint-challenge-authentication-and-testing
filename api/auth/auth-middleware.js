091f9887416838d8dda317f38eabafb3baef0572
Madcodes831 <r.medina2119@gmail.com>
2024-01-17 18:16:05 -0800

    mvp

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -3,11 +3,12 @@ const db = require("../../data/dbConfig");
 const checkSubmission = async (req, res, next) => {
   try {
     const { username, password } = req.body;
-    if (username.length === 0 || password.length === 0) {
+    if (!username || !password || username.length === 0 || password.length === 0) {
       next({ status: 400, message: "username and password required" });
     } else {
       next();
     }
+      
   } catch (err) {
     next(err);
   }
